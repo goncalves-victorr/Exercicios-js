a2c4edf85de21e98451b35229e875750ae3648c9
goncalves-victorr <victorpositivo2016@gmail.com>
2023-09-26 15:40:44 -0300

    exercicio_3

---
+++ exercicios-fund-estruturas-funcao/Exercicio_3.js
@@ -0,0 +1,7 @@
+// 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function resultado(base, expoente) {
+    
+    console.log(Math.pow(base, expoente));
+}
+resultado(2, 3)
