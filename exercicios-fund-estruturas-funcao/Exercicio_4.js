d336b62a5a016a28721db5f6116fa46be3807d1b
goncalves-victorr <victorpositivo2016@gmail.com>
2023-09-26 15:52:44 -0300

    exercicio_4

---
+++ exercicios-fund-estruturas-funcao/Exercicio_4.js
@@ -0,0 +1,8 @@
+/*04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.*/
+
+function resultado( dividendo, divisor) {
+    console.log(dividendo / divisor);
+    console.log(dividendo % divisor);
+}
+resultado(20,5)
