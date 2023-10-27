e854ad5e522982f46bd614b076c069752e3922ae
goncalves-victorr <victorpositivo2016@gmail.com>
2023-10-27 09:03:34 -0300

    oo

---
+++ objeto/oo.js
@@ -0,0 +1,16 @@
+// Código não Executável !!!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+// oo
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // .....
+    }
+}
+
+objeto.processamento()
